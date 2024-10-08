2f3e978328ef2214c2110b6e587b6dd720781d1b
Oliver Drew <oliver@oliver-drew.co.uk>
2024-10-08 12:46:54 +0100

    feat(setup): adds and enforces prettier code formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,32 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import path from "path";
-import { includeIgnoreFile } from "@eslint/compat";
+import path from 'path';
+import { includeIgnoreFile } from '@eslint/compat';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const gitIngorePath = path.resolve(__dirname, '.gitignore');
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: [
-      'node_modules'
-    ]
+    plugins: {
+      prettier: eslintPluginPrettier
+    },
+    rules: {
+      'prettier/prettier': 'error'
+    }
+  },
+  eslintConfigPrettier,
+  {
+    ignores: ['node_modules']
   },
   includeIgnoreFile(gitIngorePath)
 ];
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+const config = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none'
+};
+
+export default config;
